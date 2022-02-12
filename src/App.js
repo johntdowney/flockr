90e21ffd554594cce099d8e241aed935fad3538a
John Downey <johntdowney@gmail.com>
2022-02-12 15:28:11 -0700

    Added in some simple mouse interactions.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import Slider, { Range } from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import ResizeSensor from 'resize-sensor'
 
 function uuidv4() {
     return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
@@ -104,26 +105,43 @@ const ParticlesView = (props)=> {
                 if (p == p2) continue;
                 let distance = new Vector(p.x - p2.x, p.y - p2.y).magnitude() - (p.r + p2.r);
                 
-                let isNeighborInSight = distance > ((props.width/10) * props.config.sight);
+                let isNeighborInSight = distance > ((props.width) * props.config.sight);
                 
                 if(isNeighborInSight) continue;
                 neighborCount++;
                 avgVec.add(p2.v)
                 let isRepelled = distance < ((props.width/10) * props.config.bubble);
                 let influenceVec;
-                if (isRepelled) influenceVec = new Vector(p.x - p2.x, p.y - p2.y).scale(props.config.repellent) 
-                else influenceVec = new Vector(p2.x - p.x, p2.y - p.y).scale(props.config.attraction/10000);
+                if (isRepelled) influenceVec = new Vector(p.x - p2.x, p.y - p2.y).scale(props.config.repellent/100) 
+                else influenceVec = new Vector(p2.x - p.x, p2.y - p.y).scale(props.config.attraction/5000);
                 
                 p.v.add(influenceVec).normalize();
             }
             if(neighborCount > 0) {
                 p.v.add(avgVec.normalize().scale(0.05 * props.config.herd));
             }
+            
+            if(props.config.pointerOver) {
+//                console.log("POINTER OVER");
+                let ptr = props.config.pointer;
+                let distance = new Vector(p.x - ptr.x, p.y - ptr.y).magnitude() - p.r;
+                let isPtrInSight = distance <= ((props.width) * props.config.sight);
+                if(isPtrInSight) {
+                    p.v.add(new Vector(ptr.x - p.x, ptr.y - p.y).scale(props.config.attraction/50)).normalize();
+                    if(props.config.pointerDown) {
+                        console.log("POINTER DOWN");
+                        let ptr = props.config.pointer;
+
+                        p.v.add(new Vector(p.x - ptr.x, p.y - ptr.y).scale(props.config.attraction/5)).normalize();
+                    }
+                }
+                
+            }
         }
 
         for(let p of props.models) {
-            p.x += p.v[0] * p.s * props.config.speed;
-            p.y += p.v[1] * p.s * props.config.speed;
+            p.x += p.v[0] * p.s * (props.config.speed * 100);
+            p.y += p.v[1] * p.s * (props.config.speed * 100);
 
             while((p.x - p.r) > props.width) p.x -= props.width;
             while((p.y - p.r) > props.height) p.y -= props.height;
@@ -145,9 +163,54 @@ const ParticlesView = (props)=> {
                     <circle cx={isNaN(model.x)?0:model.x} cy={isNaN(model.y)?0:model.y} r={model.r} fill="rgba(255,255,255,0.3)"/>
                 </g>;
     });
-    return  <svg version="1.1" width={props.width} height={props.height} xmlns="http://www.w3.org/2000/svg">
+    const onMouseEnter = () => {
+//        props.config.pointerOver = true
+    }
+    const onMouseLeave = () => {
+        props.config.pointerOver = false
+    }
+    const onMouseDown = () => {
+        props.config.pointerDown = true
+    }
+    const onMouseUp = () => {
+        props.config.pointerDown = false
+    }
+    const onMouseMove = (e) => {
+//        this.props.config.pointer.x = 
+//        console.log("MOUSE OVER", e)
+        props.config.pointerOver = true
+        props.config.pointer = {x: (e.clientX / window.innerWidth) * props.width, y: (e.clientY / window.innerHeight) * props.height};
+        
+    }
+    return  <svg onMouseEnter={onMouseEnter} onMouseMove={onMouseMove} onMouseDown={onMouseDown} onMouseUp={onMouseUp} onMouseLeave={onMouseLeave} preserveAspectRatio="none" version="1.1" width={props.width} height={props.height} xmlns="http://www.w3.org/2000/svg">
                 {ps}
             </svg>;
+}
+
+class Particle {
+    constructor(worldWidth, worldHeight) {
+        
+        let radius = 3+(Math.random() * 5);
+        let x = Math.random() * worldWidth;
+        let y = Math.random() * worldHeight;
+
+        x = Math.min(Math.max(x, radius), worldWidth-radius);
+        y = Math.min(Math.max(y, radius), worldHeight-radius);
+        let dx = Math.random() - 0.5;
+        let dy = Math.random() - 0.5;
+        let speed = 1;//Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2));
+        dx /= speed;
+        dy /= speed;
+        speed /= (radius);
+        //        speed = 1;
+
+        this.id = uuidv4();
+        this.x = x;
+        this.y = y;
+        this.v = new Vector(dx, dy);
+        this.s = speed;
+        this.r = radius;
+    }
 }
 
 class Particles extends React.Component {
@@ -155,14 +218,8 @@ class Particles extends React.Component {
         super(props);
         this.config = props.config;
         this.state = JSON.parse(JSON.stringify(props.config));
-        this.models = [];
-        for(let i = 0; i < this.config.count; i++) this.models.push(this.createRandomParticle());
-        
-        this.lattice = new Lattice(this.config.latticeSize, this.config.width, this.config.height);
-        for(let i = 0; i < this.models.length; i++) {
-            this.lattice.add(this.models[i]);
-        }
-        console.log("lattice = ", this.lattice);
+        this.models = props.models;
+        this.lattice = props.lattice;
         this.onSliderChange = this.onSliderChange.bind(this);    
     }
 
@@ -180,7 +237,7 @@ class Particles extends React.Component {
         if(name === 'count') {
             value = parseInt(value);
             while(value > this.models.length) {
-                let m = this.createRandomParticle();
+                let m = new Particle(this.config.width, this.config.height);
                 this.models.push(m);
                 this.lattice.add(m);
             }
@@ -192,30 +249,12 @@ class Particles extends React.Component {
         }
         this.config[name] = value;
     }
-    createRandomParticle() {
-
-        let radius = 3+(Math.random() * 5);
-        let x = Math.random() * this.config.width;
-        let y = Math.random() * this.config.height;
-
-        x = Math.min(Math.max(x, radius), this.config.width-radius);
-        y = Math.min(Math.max(y, radius), this.config.height-radius);
-        let dx = Math.random() - 0.5;
-        let dy = Math.random() - 0.5;
-        let speed = 1;//Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2));
-        dx /= speed;
-        dy /= speed;
-        speed /= (radius);
-        //        speed = 1;
-
-        return {id:uuidv4(), x:x, y:y, v: new Vector(dx, dy), s:speed, r:radius};
-    }
 
 
     render() {
-        return  <div>
-                    <div style={{display:'flex', flexDirection:'row', fontSize:'11px', textTransform:'uppercase', justifyContent:'space-between'}}>
-                        <div style={{display:'flex', flexDirection:'column'}}>
+        return  <div className="particles-container">
+                    <div className="controls">
+                        <div>
                             <label htmlFor="speed">Count<br />{this.config.count}</label>
                             <input 
                                 id="count"
@@ -225,7 +264,27 @@ class Particles extends React.Component {
                                 value={this.config.count} 
                                 onChange={this.onSliderChange} />
                         </div>
-                        <div style={{display:'flex', flexDirection:'column'}}>
+                        <div>
+                            <label htmlFor="speed">Speed<br />{this.config.speed}</label>
+                            <input 
+                                id="speed"
+                                name="speed" 
+                                type="range" 
+                                min="0" max="1" step="0.001"
+                                value={this.config.speed} 
+                                onChange={this.onSliderChange} />
+                        </div>
+                        <div>
+                            <label htmlFor="sight">Sight Radius<br />{this.config.sight}</label>
+                            <input 
+                                id="sight"
+                                name="sight" 
+                                type="range" 
+                                min="0" max="1" step="0.001"
+                                value={this.config.sight} 
+                                onChange={this.onSliderChange} />
+                        </div>
+                        <div>
                             <label htmlFor="attraction">Attraction<br />{this.config.attraction}</label>
                             <input 
                                 id="attraction"
@@ -235,7 +294,7 @@ class Particles extends React.Component {
                                 value={this.config.attraction} 
                                 onChange={this.onSliderChange} />
                         </div>
-                        <div style={{display:'flex', flexDirection:'column'}}>
+                        <div>
                             <label htmlFor="repellent">Repellent<br />{this.config.repellent}</label>
                             <input 
                                 id="repellent"
@@ -246,7 +305,7 @@ class Particles extends React.Component {
                                 onChange={this.onSliderChange} />
                         </div>
 
-                        <div style={{display:'flex', flexDirection:'column'}}>
+                        <div>
                             <label htmlFor="bubble">Personal Bubble<br />{this.config.bubble}</label>
                             <input 
                                 id="bubble"
@@ -256,7 +315,7 @@ class Particles extends React.Component {
                                 value={this.config.bubble} 
                                 onChange={this.onSliderChange} />
                         </div>
-                        <div style={{display:'flex', flexDirection:'column'}}>
+                        <div>
                             <label htmlFor="herd">Herd Mentality<br />{this.config.herd}</label>
 
                             <input 
@@ -268,7 +327,7 @@ class Particles extends React.Component {
                                 onChange={this.onSliderChange} />
                         </div>
                     </div>
-                    <div style={{backgroundColor:'rgba(0,0,0,0.25)'}}>
+                    <div className="particles-view">
                         <ParticlesView width={this.config.width} height={this.config.height} models={this.models} config={this.config}/>
                     </div>
                 </div>;
@@ -276,27 +335,34 @@ class Particles extends React.Component {
 }
 
 
-
 function App() {
     let config = {
-        count:40,
-        height:600,
-        width:800,
+        count:30,
+        width: window.innerWidth,
+        height: window.innerHeight,
         attraction:0.125,
         bubble: 0.035,
-        sight:4,
+        sight:0.4,
         repellent: 0.028,
         herd:0.45,
-        speed:25,
-        latticeSize: 10
+        speed:0.15,
+        latticeSize: 10,
+        pointer:{x:0,y:0},
+        pointerDown:false
     }
-
+    
+    const models = [];
+    for(let i = 0; i < config.count; i++) models.push(new Particle(config.width, config.height));
+    const lattice = new Lattice(config.latticeSize, config.width, config.height);
+    
+    for(let i = 0; i < models.length; i++) {
+        lattice.add(models[i]);
+    }
+    console.log("lattice = ", lattice);
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <Particles config={config} />
-            </header>
+        <div id="app-container" className="App">
+            <Particles config={config} models={models} lattice={lattice} />
         </div>
     );
 }
